6be247f1c3cecb266b0a40c9e1d17fdfff4e2478
minorDeveloper <theminordeveloper@outlook.com>
2023-03-10 01:09:03 +0000

    Asio send udp packet

--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@
 #include <iostream>
 #include <asio.hpp>
 
-void print(const asio::error_code& /*e*/) {
-    std::cout << "Hello, World!" << std::endl;
-}
+using asio::ip::udp;
 
 int main() {
-    asio::io_context io;
+    try {
+        asio::io_context io_context;
+        udp::resolver resolver(io_context);
+        udp::endpoint receiver_endpoint =
+                *resolver.resolve(udp::v4(), "192.168.1.1", "daytime").begin();
 
-    asio::steady_timer t(io, asio::chrono::seconds(5));
+        udp::socket socket(io_context);
+        socket.open(udp::v4());
 
-    t.async_wait(&print);
-    io.run();
+        char send_buf[1] = {0};
+        socket.send_to(asio::buffer(send_buf), receiver_endpoint);
 
-    std::cout << "This should happen first" << std::endl;
+        char recv_buf[128];
+        udp::endpoint sender_endpoint;
+        size_t len = socket.receive_from(
+                asio::buffer(recv_buf), sender_endpoint
+                );
+
+        std::cout.write(recv_buf, len);
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
